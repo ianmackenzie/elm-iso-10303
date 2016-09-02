6e2ec3617621a9d6ca556552e2271e48ddaf0f8a
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-09-02 17:32:04 +1000

    Remove toCodePoints
    Has been moved to elm-community/string-extra

--- src/OpenSolid/StepFile.elm
+++ src/OpenSolid/StepFile.elm
@@ -28,69 +28,6 @@ hexEncode size value =
         String.concat characters
 
 
-toCodePoints : String -> List Int
-toCodePoints string =
-    let
-        combineSurrogates codeUnits =
-            case codeUnits of
-                [] ->
-                    []
-
-                first :: afterFirst ->
-                    -- We have at least one code unit - might be a code point
-                    -- itself, or the leading code unit of a surrogate pair
-                    if first >= 0 && first <= 0xD7FF then
-                        -- First code unit is in BMP (and is therefore a valid
-                        -- UTF-32 code point), use it as is and continue with
-                        -- remaining code units
-                        first :: combineSurrogates afterFirst
-                    else if first >= 0xD800 && first <= 0xDBFF then
-                        -- First code unit is a leading surrogate
-                        case afterFirst of
-                            [] ->
-                                -- Should never happen - leading surrogate with
-                                -- no following code unit, discard it
-                                []
-
-                            second :: afterSecond ->
-                                -- Good, there is a following code unit (which
-                                -- should be a trailing surrogate)
-                                if second >= 0xDC00 && second <= 0xDFFF then
-                                    -- Second code unit is a valid trailing
-                                    -- surrogate
-                                    let
-                                        -- Reconstruct UTF-32 code point from
-                                        -- surrogate pair
-                                        codePoint =
-                                            0x00010000
-                                                + ((first - 0xD800) * 1024)
-                                                + (second - 0xDC00)
-                                    in
-                                        -- Continue with following code units
-                                        codePoint
-                                            :: combineSurrogates afterSecond
-                                else
-                                    -- Should never happen - second code unit is
-                                    -- not a valid trailing surrogate, skip the
-                                    -- leading surrogate and continue with
-                                    -- remaining code units
-                                    combineSurrogates afterFirst
-                    else if first >= 0xE000 && first <= 0xFFFF then
-                        -- First code unit is in BMP (and is therefore a valid
-                        -- UTF-32 code point), use it as is and continue with
-                        -- remaining code units
-                        first :: combineSurrogates afterFirst
-                    else
-                        -- Should never happen - first code unit is invalid,
-                        -- skip it and continue with remaining code units
-                        combineSurrogates afterFirst
-
-        allCodeUnits =
-            List.map Char.toCode (String.toList string)
-    in
-        combineSurrogates allCodeUnits
-
-
 encode : String -> String
 encode string =
     let
