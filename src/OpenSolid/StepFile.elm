3d5d3fe4b6d62d665f1fef2b3db1d23f4210f437
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-12-29 15:54:57 -0500

    Refactor code points conversion
    Use toCodePoints from string-extra, move codePointToString to top level
    to prevent recreation

--- src/OpenSolid/StepFile.elm
+++ src/OpenSolid/StepFile.elm
@@ -30,21 +30,24 @@ hexEncode size value =
         String.concat characters
 
 
+codePointToString : Int -> String
+codePointToString codePoint =
+    if (codePoint >= 0 && codePoint <= 0x1F) then
+        "\\X\\" ++ hexEncode 2 codePoint
+    else if (codePoint >= 0x20 && codePoint <= 0x7E) then
+        String.fromChar (Char.fromCode codePoint)
+    else if (codePoint >= 0x7F && codePoint <= 0xFF) then
+        "\\X\\" ++ hexEncode 2 codePoint
+    else if (codePoint >= 0x0100 && codePoint <= 0xFFFF) then
+        "\\X2\\" ++ hexEncode 4 codePoint ++ "\\X0\\"
+    else if (codePoint >= 0x00010000 && codePoint <= 0x0010FFFF) then
+        "\\X4\\" ++ hexEncode 8 codePoint ++ "\\X0\\"
+    else
+        ""
+
+
 encode : String -> String
-encode string =
-    let
-        codePointToString codePoint =
-            if (codePoint >= 0 && codePoint <= 0x1F) then
-                "\\X\\" ++ hexEncode 2 codePoint
-            else if (codePoint >= 0x20 && codePoint <= 0x7E) then
-                String.fromChar (Char.fromCode codePoint)
-            else if (codePoint >= 0x7F && codePoint <= 0xFF) then
-                "\\X\\" ++ hexEncode 2 codePoint
-            else if (codePoint >= 0x0100 && codePoint <= 0xFFFF) then
-                "\\X2\\" ++ hexEncode 4 codePoint ++ "\\X0\\"
-            else if (codePoint >= 0x00010000 && codePoint <= 0x0010FFFF) then
-                "\\X4\\" ++ hexEncode 8 codePoint ++ "\\X0\\"
-            else
-                ""
-    in
-        String.concat (List.map codePointToString (toCodePoints string))
+encode =
+    String.Extra.toCodePoints
+        >> List.map codePointToString
+        >> String.concat
