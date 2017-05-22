bb158d9ac06ed52ae09f0897507e3cd2dbb42b56
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-05-22 13:40:52 -0400

    Finish initial attribute parsing

--- src/OpenSolid/Step/Parse.elm
+++ src/OpenSolid/Step/Parse.elm
@@ -6,11 +6,22 @@ import OpenSolid.Step.Format as Format
 import Parser exposing (Parser, (|.), (|=))
 import Parser.LanguageKit
 import Char
+import String
+import String.Extra as String
+import Bitwise
 
 
 type ParsedAttribute
-    = EvaluatedAttribute Types.Attribute
-    | UnevaluatedReference Int
+    = ParsedDefaultAttribute
+    | ParsedNullAttribute
+    | ParsedIntAttribute Int
+    | ParsedFloatAttribute Float
+    | ParsedStringAttribute String
+    | ParsedBinaryAttribute String
+    | ParsedEnumAttribute Types.EnumName
+    | ParsedReference Int
+    | ParsedTypedAttribute Types.TypeName ParsedAttribute
+    | ParsedAttributeList (List ParsedAttribute)
 
 
 type alias ParsedEntity =
@@ -83,8 +94,8 @@ whitespace =
             |> Parser.andThen (always (Parser.succeed ()))
 
 
-typeName : Parser String
-typeName =
+keyword : Parser String
+keyword =
     let
         validFirstCharacter character =
             Char.isUpper character || character == '_'
@@ -98,32 +109,126 @@ typeName =
             )
 
 
-int : Parser Int
-int =
+typeName : Parser Types.TypeName
+typeName =
+    keyword |> Parser.map Types.TypeName
+
+
+enumName : Parser Types.EnumName
+enumName =
+    keyword |> Parser.map Types.EnumName
+
+
+isBasic : Char -> Bool
+isBasic character =
+    let
+        code =
+            Char.toCode character
+    in
+        (code >= 0x20 && code <= 0x7E)
+            && (character /= '\'')
+            && (character /= '\\')
+
+
+isHexCharacter : Char -> Bool
+isHexCharacter character =
+    (character >= '0' && character <= '9')
+        || (character >= 'A' && character <= 'F')
+
+
+hexDigit : Parser Int
+hexDigit =
     Parser.oneOf
-        [ Parser.int
-        , Parser.symbol "+"
-            |> Parser.andThen (\() -> Parser.int)
-        , Parser.symbol "-"
-            |> Parser.andThen (\() -> Parser.int)
-            |> Parser.map negate
+        [ Parser.symbol "0" |> Parser.map (\() -> 0)
+        , Parser.symbol "1" |> Parser.map (\() -> 1)
+        , Parser.symbol "2" |> Parser.map (\() -> 2)
+        , Parser.symbol "3" |> Parser.map (\() -> 3)
+        , Parser.symbol "4" |> Parser.map (\() -> 4)
+        , Parser.symbol "5" |> Parser.map (\() -> 5)
+        , Parser.symbol "6" |> Parser.map (\() -> 6)
+        , Parser.symbol "7" |> Parser.map (\() -> 7)
+        , Parser.symbol "8" |> Parser.map (\() -> 8)
+        , Parser.symbol "9" |> Parser.map (\() -> 9)
+        , Parser.symbol "A" |> Parser.map (\() -> 10)
+        , Parser.symbol "B" |> Parser.map (\() -> 11)
+        , Parser.symbol "C" |> Parser.map (\() -> 12)
+        , Parser.symbol "D" |> Parser.map (\() -> 13)
+        , Parser.symbol "E" |> Parser.map (\() -> 14)
+        , Parser.symbol "F" |> Parser.map (\() -> 15)
         ]
 
 
-float : Parser Float
-float =
-    Parser.oneOf
-        [ Parser.float
-        , Parser.symbol "+"
-            |> Parser.andThen (\() -> Parser.float)
-        , Parser.symbol "-"
-            |> Parser.andThen (\() -> Parser.float)
-            |> Parser.map negate
-        ]
+x0 : Int -> Int -> String
+x0 high low =
+    String.fromCodePoints [ Bitwise.shiftLeftBy 4 high + low ]
 
 
+x2 : List ( Int, Int, Int, Int ) -> String
+x2 hexDigits =
+    let
+        codePoint ( a, b, c, d ) =
+            d
+                + (Bitwise.shiftLeftBy 4 c)
+                + (Bitwise.shiftLeftBy 8 b)
+                + (Bitwise.shiftLeftBy 12 a)
+    in
+        String.fromCodePoints (List.map codePoint hexDigits)
 
--- TODO add string, binary, enum, reference, typed attribute, attribute list
+
+x4 : List ( Int, Int, Int, Int, Int, Int ) -> String
+x4 hexDigits =
+    let
+        codePoint ( a, b, c, d, e, f ) =
+            f
+                + (Bitwise.shiftLeftBy 4 e)
+                + (Bitwise.shiftLeftBy 8 d)
+                + (Bitwise.shiftLeftBy 12 c)
+                + (Bitwise.shiftLeftBy 16 b)
+                + (Bitwise.shiftLeftBy 20 a)
+    in
+        String.fromCodePoints (List.map codePoint hexDigits)
+
+
+string : Parser String
+string =
+    Parser.succeed String.concat
+        |. Parser.symbol "'"
+        |= Parser.repeat Parser.zeroOrMore
+            (Parser.oneOf
+                [ Parser.symbol "''" |> Parser.map (\() -> "'")
+                , Parser.symbol "\\\\" |> Parser.map (\() -> "\\")
+                , Parser.succeed x0
+                    |. Parser.symbol "\\X\\"
+                    |= hexDigit
+                    |= hexDigit
+                , Parser.succeed x2
+                    |. Parser.symbol "\\X2\\"
+                    |= Parser.repeat Parser.oneOrMore
+                        (Parser.succeed (,,,)
+                            |= hexDigit
+                            |= hexDigit
+                            |= hexDigit
+                            |= hexDigit
+                        )
+                    |. Parser.symbol "\\X0\\"
+                , Parser.succeed x4
+                    |. Parser.symbol "\\X4\\"
+                    |= Parser.repeat Parser.oneOrMore
+                        (Parser.succeed (,,,,,)
+                            |. Parser.symbol "0"
+                            |. Parser.symbol "0"
+                            |= hexDigit
+                            |= hexDigit
+                            |= hexDigit
+                            |= hexDigit
+                            |= hexDigit
+                            |= hexDigit
+                        )
+                    |. Parser.symbol "\\X0\\"
+                , Parser.keep Parser.oneOrMore isBasic
+                ]
+            )
+        |. Parser.symbol "'"
 
 
 attribute : Parser ParsedAttribute
@@ -131,13 +236,11 @@ attribute =
     let
         defaultAttribute =
             Parser.symbol "*"
-                |> Parser.map
-                    (\() -> (EvaluatedAttribute Types.DefaultAttribute))
+                |> Parser.map (\() -> ParsedDefaultAttribute)
 
         nullAttribute =
             Parser.symbol "$"
-                |> Parser.map
-                    (\() -> (EvaluatedAttribute Types.NullAttribute))
+                |> Parser.map (\() -> ParsedNullAttribute)
 
         numericAttribute =
             Parser.oneOf
@@ -150,15 +253,54 @@ attribute =
                 |> Parser.sourceMap
                     (\rawString parsedFloat ->
                         if String.contains "." rawString then
-                            EvaluatedAttribute
-                                (Types.FloatAttribute parsedFloat)
+                            ParsedFloatAttribute parsedFloat
                         else
-                            EvaluatedAttribute
-                                (Types.IntAttribute (round parsedFloat))
+                            ParsedIntAttribute (round parsedFloat)
                     )
+
+        stringAttribute =
+            string |> Parser.map ParsedStringAttribute
+
+        binaryAttribute =
+            Parser.succeed identity
+                |. Parser.symbol "\""
+                |= Parser.keep Parser.oneOrMore isHexCharacter
+                |. Parser.symbol "\""
+                |> Parser.map ParsedBinaryAttribute
+
+        enumAttribute =
+            Parser.succeed identity
+                |. Parser.symbol "."
+                |= enumName
+                |. Parser.symbol "."
+                |> Parser.map ParsedEnumAttribute
+
+        unevaluatedReference =
+            Parser.succeed identity
+                |. Parser.symbol "#"
+                |= Parser.int
+                |> Parser.map ParsedReference
+
+        typedAttribute =
+            Parser.succeed ParsedTypedAttribute
+                |= typeName
+                |. Parser.symbol "("
+                |= Parser.lazy (\() -> attribute)
+                |. Parser.symbol ")"
+
+        attributeList =
+            Parser.LanguageKit.tuple whitespace
+                (Parser.lazy (\() -> attribute))
+                |> Parser.map ParsedAttributeList
     in
         Parser.oneOf
             [ defaultAttribute
             , nullAttribute
             , numericAttribute
+            , stringAttribute
+            , binaryAttribute
+            , enumAttribute
+            , unevaluatedReference
+            , typedAttribute
+            , attributeList
             ]
