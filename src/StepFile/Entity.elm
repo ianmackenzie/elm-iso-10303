c22a4c3c302995268054ef8902b8e0a65cfa621e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-07-16 14:21:59 -0400

    Tweak doc wording
    Flows a bit better

--- src/StepFile/Entity.elm
+++ src/StepFile/Entity.elm
@@ -7,7 +7,7 @@ module StepFile.Entity
         )
 
 {-| An `Entity` represents a single entity stored in the data section of a STEP
-file. An entity may be a point, a curve, an assembly, or even an entire
+file. An entity may be a point, a curve, a part, an assembly, or even an entire
 building. Every entity has a type and a list of attributes (which themselves be
 references to other entities).
 
