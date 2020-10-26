8f0844ef1bd39b76f017f620a438574ad233f79e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-10-25 22:42:38 -0400

    Document and expose TypeName/EnumValue modules

--- src/Step/EnumValue.elm
+++ src/Step/EnumValue.elm
@@ -1,19 +1,31 @@
 module Step.EnumValue exposing (EnumValue, fromString, toString)
 
-{-| -}
+{-|
+
+@docs EnumValue, fromString, toString
+
+-}
 
 
-{-| A capitalized enum name like "STEEL", with no leading or trailing periods.
+{-| A capitalized enum value such as "STEEL" or "METRE".
 -}
 type EnumValue
     = EnumValue String
 
 
+{-| Construct an enum value from a string. The string will be capitalized and
+will have periods removed.
+-}
 fromString : String -> EnumValue
 fromString string =
     EnumValue (string |> String.toUpper |> String.replace "." "")
 
 
+{-| Convert an enum value to a string. The result will always be capitalized
+and will not have a leading/trailing periods (enum values are encoded in STEP
+files using leading and trailing periods, but those periods are considered to be
+an encoding detail and not part of the enum value).
+-}
 toString : EnumValue -> String
 toString (EnumValue string) =
     string
--- src/Step/TypeName.elm
+++ src/Step/TypeName.elm
@@ -1,6 +1,10 @@
 module Step.TypeName exposing (TypeName, fromString, toString)
 
-{-| -}
+{-|
+
+@docs TypeName, fromString, toString
+
+-}
 
 
 {-| A capitalized type name like "IFCWALL".
@@ -9,11 +13,15 @@ type TypeName
     = TypeName String
 
 
+{-| Construct a type name from a string. This wil capitalize the string.
+-}
 fromString : String -> TypeName
 fromString string =
     TypeName (String.toUpper string)
 
 
+{-| Convert a type name to a string. The result will always be capitalized.
+-}
 toString : TypeName -> String
 toString (TypeName string) =
     string
